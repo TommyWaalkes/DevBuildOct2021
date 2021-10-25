a05d890272801b5189c349600d55ea99f66d1dfe
Tommy Waalkes <Tommy@grandcircus.co>
2021-10-25 12:03:13 -0400

    Added Blockbuster

---
+++ Blockbuster/Blockbuster/Blockbuster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockbuster
+{
+
+    //This stores and allows us to checkout our movies 
+    //This is what I like to call a runner class since its main job 
+    //is to manage and delegate to other classes
+    class Blockbuster
+    {
+        List<Movie> Movies { get; set; } = new List<Movie>();
+
+        //We'll use the constructor to fill our movie list
+        public Blockbuster()
+        {
+            Movies.Add(new DVD("28 Days Later", Genre.Horror, 130, "Infected attack", "He Wakes up from a coma"));
+            Movies.Add(new VHS("Alladin", Genre.Action, 90, "Opening Song", "Alladin steals bread for the kids", "A whole new world"));
+        }
+    }
+}
---
+++ Blockbuster/Blockbuster/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Blockbuster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie m = new Movie("Shrek", Genre.Comedy, 105, "Get out of my swamp", "Donkey shows up", "They visit the dragon");
+            // m.PrintInfo();
+            Console.WriteLine(m);
+            m.PrintScenes();
+
+            VHS d = new VHS("Tommy Boy", Genre.Comedy, 120, "He flunks college", "He sells stuff", "David spade is sarcastic");
+
+            while (true)
+            {
+                d.Play();
+            }
+        }
+    }
+}
