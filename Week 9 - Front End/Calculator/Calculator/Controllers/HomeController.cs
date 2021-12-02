6d4c8948db14959c8927299b383502719f7db38a
Tommy Waalkes <Tommy@grandcircus.co>
2021-12-02 13:17:27 -0500

    Added Calculator App

---
+++ Week 9 - Front End/Calculator/Calculator/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        public IActionResult Result(Calc c)
+        {
+            string name = "tommy";
+
+            if(name.Length == 50)
+            {
+
+            }
+            double result = 0; 
+            if(c.Operation == Operation.plus)
+            {
+                result = c.Num1 + c.Num2;
+            }
+            else if (c.Operation == Operation.minus)
+            {
+                result = c.Num1 - c.Num2;
+            }
+            else if (c.Operation == Operation.multiply)
+            {
+                result = c.Num1 * c.Num2;
+            }
+            else
+            {
+                result = c.Num1 / c.Num2;
+            }
+            c.Result = result;
+            return View(c);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week 9 - Front End/Calculator/Calculator/Models/Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public enum Operation
+    {
+        plus, 
+        minus, 
+        multiply, 
+        divide
+    }
+
+    //Models are containers for data 
+    public class Calc
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public Operation Operation { get; set; }
+        public double Result { get; set; }
+    }
+}
