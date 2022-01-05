c0461cbe1449249cf1d7f54c33da6e50ff2ab7cc
Tommy Waalkes <Tommy@grandcircus.co>
2022-01-05 10:46:48 -0500

    Added Deck of Cards Example

---
+++ Week 12 - APIs/DeckOfCards/DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DeckOfCards.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        DeckOfCardsDAL cardDAL = new DeckOfCardsDAL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewDeck()
+        {
+            Deck d = cardDAL.CreateDeck();
+            return View(d);
+        }
+
+        public IActionResult DrawCards(int DrawCount, string DeckId)
+        {
+            Draw d = cardDAL.DrawCards(DeckId, DrawCount);
+            return View(d);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week 12 - APIs/DeckOfCards/DeckOfCards/Models/DeckOfCardsDAL.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Models
+{
+    public class DeckOfCardsDAL
+    {
+        public Deck CreateDeck()
+        {
+            string url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+            //This makes the request at our URL 
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //This grabs the file out of the response
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string JSON = rd.ReadToEnd();
+            rd.Close();
+
+            Deck d = JsonConvert.DeserializeObject<Deck>(JSON);
+
+            return d;
+        }
+
+        public Draw DrawCards(string deckId, int drawCount)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count={drawCount}";
+            //This makes the request at our URL 
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //This grabs the file out of the response
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string JSON = rd.ReadToEnd();
+            rd.Close();
+
+            Draw d = JsonConvert.DeserializeObject<Draw>(JSON);
+
+            return d;
+        }
+
+    }
+}
