2a53d53897775e84c1adb0ae3274dadd43624ef6
Tommy Waalkes <Tommy@grandcircus.co>
2022-01-06 15:44:26 -0500

    Finished Reddit API Example

---
+++ Week 12 - APIs/RedditAPI/RedditAPI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedditAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        RedditDAL rDAL = new RedditDAL();
+        public IActionResult Index()
+        {
+            Child c = rDAL.GetPost(4);
+            return View(c);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week 12 - APIs/RedditAPI/RedditAPI/Models/RedditDAL.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RedditAPI.Models
+{
+    public class RedditDAL
+    {
+        public Child GetPost(int index)
+        {
+            string url = "https://www.reddit.com/r/aww/.json";
+
+                //Next we need to create our request 
+                HttpWebRequest request = WebRequest.CreateHttp(url);
+
+                //Next if your API needs any kind of login or key, that may go here 
+                //SWAPI doesn't need anything so we're good
+
+                //Now we're ready to send off our request and grab the server's response 
+                //Inside our response, the resulting data lives 
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                //Pull the result into a stream reader which will then give us a string 
+                StreamReader rd = new StreamReader(response.GetResponseStream());
+
+                //Grab our response string - read to end starts at the top of our response file and returns each line 
+                //until it hits the end. 
+                string result = rd.ReadToEnd();
+
+            //This line converts our JSON string into a person object automatically 
+            SubReddit sr = JsonConvert.DeserializeObject<SubReddit>(result);
+            return sr.data.children[index];
+
+            
+        }
+    }
+}
