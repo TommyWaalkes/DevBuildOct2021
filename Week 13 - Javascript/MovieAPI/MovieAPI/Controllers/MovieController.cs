f03403e01f35434d881c531941b99424aaae46e0
Tommy Waalkes <Tommy@grandcircus.co>
2022-01-10 12:01:39 -0500

    added Movie Lab

---
+++ Week 13 - Javascript/MovieAPI/MovieAPI/Controllers/MovieController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        MovieDAL md = new MovieDAL();
+
+        [HttpGet]
+        public List<Movie> GetMovies()
+        {
+            return md.GetMovies();
+        }
+
+        [HttpGet("RandomList/{quantity}")]
+        public List<Movie> GetRandomMovies(int quantity)
+        {
+            //Grab the full list of movies 
+            //Randomly keep picking movies until we match quantity 
+
+            //if quantity > movies.count, return full list of movies 
+            List<Movie> movies = md.GetMovies();
+
+            if (movies.Count <= quantity)
+            {
+                return movies;
+            }
+            else
+            {
+                List<Movie> output = new List<Movie>();
+
+                while (output.Count < quantity)
+                {
+                    Random rng = new Random();
+                    int pick = rng.Next(0, movies.Count);
+                    output.Add(movies[pick]);
+                    movies.RemoveAt(pick);
+                }
+
+                return output;
+            }
+        }
+
+        [HttpGet("Categories")]
+        public List<string> GetCategories()
+        {
+            return md.GetCategories();
+        }
+
+        [HttpGet("GetMovie/{title}")]
+        public Movie GetMovie(string title)
+        {
+            List<Movie> movies = md.GetMovies();
+            List<Movie> filtered = movies.Where(x => x.Title == title).ToList();
+            if (filtered.Count > 0)
+            {
+              
+                return filtered.First();
+            }
+            else
+            {
+                Movie error = new Movie();
+                error.Title = "No movie found with that title: " + title;
+                return error;
+            }
+        }
+
+        [HttpGet("Search/{keyword}")]
+        public List<Movie> SearchMovieByTitle(string keyword)
+        {
+            List<Movie> movies = md.GetMovies();
+            List<Movie> filtered = movies.Where(x => x.Title.Contains(
+                keyword.ToLower()) || x.Title.Contains(keyword.ToUpper())).ToList();
+            if (filtered.Count > 0)
+            {
+
+                return filtered;
+            }
+            else
+            {
+                Movie error = new Movie();
+                error.Title = "No movie found with that title: " + keyword;
+
+                List<Movie> output = new List<Movie>();
+                output.Add(error);
+                return output;
+            }
+        }
+    }
+}
---
+++ Week 13 - Javascript/MovieAPI/MovieAPI/MovieDAL.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI
+{
+    public class MovieDAL
+    {
+        public List<Movie> GetMovies()
+        {
+            string sql = "select * from Movies";
+
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                connect.Open();
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                connect.Close();
+                return movies;
+            }
+        }
+
+        public List<string> GetCategories()
+        {
+
+            string sql = "select distinct genre from Movies";
+
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                connect.Open();
+                List<string> categories = connect.Query<string>(sql).ToList();
+                connect.Close();
+                return categories;
+            }
+        }
+    }
+}
